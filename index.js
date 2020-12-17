77b0667c16f2bc8cd02e4abf0cb933bf6ef33b44
yurukei20 <60683849+traveler20@users.noreply.github.com>
2020-12-17 11:12:55 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 // DOM
+//index.htmlのタグを変数に
 const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists");
 
 // 関数（メソッド）
 function addList(user) {
-  const list = document.createElement("li");
-  list.innerText = user.name;
-  lists.appendChild(list);
+	//liタグを追加する変数を定義
+	const list = document.createElement("li");
+	//追加したliタグ内のテキストを変数に
+	list.innerText = user.name;
+	//追加したliタグを#listsに追加する
+	lists.appendChild(list);
 }
 
+//上の関数の引数であるuserを返す関数
 async function getUsers() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await res.json();
-  return users;
+	//APIを取得して変数に定義
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const users = await res.json();
+	return users;
 }
 
 async function listUsers() {
-  const users = await getUsers();
-  users.forEach(addList);
+	const users = await getUsers();
+	users.forEach(addList);
 }
 
 // イベント
